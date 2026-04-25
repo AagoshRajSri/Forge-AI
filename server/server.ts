260dfceda3068c836ce0274b9f74b874e285bad9
AagoshRajSri <aagoshsrivastava10@gmail.com>
2026-04-25 21:15:51 +0530

    fix: express 5 named wildcard route

--- server/server.ts
+++ server/server.ts
@@ -44,7 +44,7 @@ app.use("/api/project", projectRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../client/dist")));
-  app.get("/(.*)", (req: Request, res: Response) => {
+  app.get("/:path*", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
   });
 } else {
